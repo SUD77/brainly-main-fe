d26660a7da70b41042f286ef0bd3bd7b3dc3f748
SUD77 <jaiswalsudhanshu20@gmail.com>
2025-07-19 01:40:19 +0530

    feat : completed button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import './App.css'
+import "./App.css";
+import { Button } from "./components/Button";
+import { PlusIcon } from "./icons/PlusIcon";
+import { ShareIcon } from "./icons/ShareIcon";
 
 function App() {
- return <div className='bg-red-300'>
-  hi there
- </div>
+  return (
+    <div>
+      <Button
+        variant="primary"
+        text="Add Content"
+        startIcon={<PlusIcon />}
+      ></Button>
+      <Button
+        variant="secondary"
+        text="Share Brain"
+        startIcon={<ShareIcon />}
+      ></Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
