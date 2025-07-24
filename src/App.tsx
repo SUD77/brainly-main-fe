cf147ed47c6ab0a3e2e77c4ea0709e8d128e5bc9
SUD77 <jaiswalsudhanshu20@gmail.com>
2025-07-24 09:42:27 +0530

    feat : added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Signin } from "./pages/Signin";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup } from "./pages/Signup";
 import { Dashboard } from "./pages/dashboard";
 
-
-
 function App() {
-  return <Dashboard/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
