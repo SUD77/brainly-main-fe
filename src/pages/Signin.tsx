61ede80c3e6d6e91949ab222916beacc22cfe50d
SUD77 <jaiswalsudhanshu20@gmail.com>
2025-07-26 11:18:00 +0530

    feat : compeleted signin logic with backend

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,15 +1,36 @@
+import { useRef } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export function Signin() {
+
+  const usernameRef = useRef<HTMLInputElement>();
+  const passwordRef = useRef<HTMLInputElement>();
+
+  async function signin() {
+    const username = usernameRef.current?.value;
+    console.log(username);
+    const password = passwordRef.current?.value;
+    // axios.post(`${BACKEND_URL}`)
+    const response = await axios.post(BACKEND_URL + "/api/v1/signin", {
+        username,
+        password,
+    });
+   const jwt=response.data.token;
+   localStorage.setItem("token",jwt);
+
+   //redirect the user to the dashboard
+  }
   return (
     <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
       <div className="bg-white border min-w-48 p-8 rounded-xl">
-        <Input placeholder="Username" />
-        <Input placeholder="Password" />
+        <Input reference={usernameRef} placeholder="Username" />
+        <Input reference={passwordRef} placeholder="Password" />
 
         <div className="flex justify-center pt-4">
-          <Button loading={false} variant="primary" text="Signin" fullWidth={true}/>
+          <Button onClick={signin} loading={false} variant="primary" text="Signin" fullWidth={true}/>
         </div>
       </div>
     </div>
